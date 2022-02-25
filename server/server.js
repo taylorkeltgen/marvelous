cb2d73590ceb36227c7cdadd707c672c6c20010e
Zak Krutikov <zak.krutikov@gmail.com>
2022-02-24 22:27:16 -0500

    continued working on server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const { assertValidExecutionArguments } = require('graphql/execution/execute');
+require('dotenv').config();
 //const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const md5 = require('md5');
+const privateKey = process.env.PRIVATE_KEY;
+const publicKey = process.env.PUBLIC_KEY;
+const axios = require('axios').default;
+const timeStamp = new Date().getTime();
+const token = md5(timeStamp, privateKey, publicKey);
+const BASE_URL = 'https://gateway.marvel.com/v1/public/';
+const testUrl = `https://gateway.marvel.com/v1/public/characters/ts=${timeStamp}&apikey=${publicKey}&hash=${token}`;
 
 // const { typeDefs, resolvers } = require('./schemas');
 // const { authMiddleware } = require('./utils/auth');
 // const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -15,12 +24,12 @@ app.use(express.json());
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-app.post('/home',async (req, res) => {
- let result = await axios.get("http://whateverapiurlis${process.env.MarvelToken}?${req.body.superHeroName}")
-
- res.json(result)
-} )
-
+app.get('/',async (req, res) => {
+ let result = await axios.get(testUrl)
+ .then (function (res) {
+  res.json(result);
+ });
+})
 
 // const startServer = async () => {
 //   const server = new ApolloServer({
@@ -38,13 +47,13 @@ app.post('/home',async (req, res) => {
 
 
 // Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 // db.once('open', () => {
 //   app.listen(PORT, () => {
