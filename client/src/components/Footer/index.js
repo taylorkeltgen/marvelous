1c53fe0ee6250e354412905bdf1881ebff0459dd
Bryan A <bryanacostadelacruz@gmail.com>
2022-02-25 05:46:18 -0600

    Hero list component, basic css. footer classname --> className typo

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,28 +4,32 @@ import github from "../../assets/github-11-32.png";
 import marvel from "../../assets/marvel-logo.png";
 
 export default function Footer() {
-  return (
-    <div classname="container">
-      <footer className="row p-4">
-        <div className="col-9">
-          <p>
-            Data provided by Marvel. &copy;{new Date().getFullYear()} Marvel
-          </p>
-          <p>
-            Created by Kevin Aboy, Bryan Acosta,<br />  Sabrina Elkins, Taylor
-            Keltgen, Zak Krutikov
-          </p>
-        </div>
-        <div className="col-3 links">
-          <p>LINKS</p>
-          <a href="https://github.com/taylorkeltgen/marvelous">
-            <img src={github}></img>
-          </a>
-          <a href="https://developer.marvel.com/">
-            <img src={marvel} className="marvel-logo"></img>
-          </a>
-        </div>
-      </footer>
-    </div>
-  );
+	return (
+		<div className="container-fluid">
+			<footer className="row p-4">
+				<div className="col-9">
+					<p>
+						Data provided by Marvel. &copy;{new Date().getFullYear()} Marvel
+					</p>
+					<p>
+						Created by Kevin Aboy, Bryan Acosta,
+						<br /> Sabrina Elkins, Taylor Keltgen, Zak Krutikov
+					</p>
+				</div>
+				<div className="col-3 links">
+					<p>LINKS</p>
+					<a href="https://github.com/taylorkeltgen/marvelous">
+						<img src={github} alt="github repository"></img>
+					</a>
+					<a href="https://developer.marvel.com/">
+						<img
+							src={marvel}
+							className="marvel-logo"
+							alt="Marvel api documentation"
+						></img>
+					</a>
+				</div>
+			</footer>
+		</div>
+	);
 }
