3f615ac56b5a7cbd4d873ff84e5082a6671b6701
Zak Krutikov <zak.krutikov@gmail.com>
2022-02-25 22:30:20 -0500

    worked on server.js

--- server/server.js
+++ server/server.js
@@ -7,29 +7,41 @@ const md5 = require('md5');
 const privateKey = process.env.PRIVATE_KEY;
 const publicKey = process.env.PUBLIC_KEY;
 const axios = require('axios').default;
-const timeStamp = new Date().getTime();
-const token = md5(timeStamp, privateKey, publicKey);
-const BASE_URL = 'https://gateway.marvel.com/v1/public/';
-const testUrl = `https://gateway.marvel.com/v1/public/characters/ts=${timeStamp}&apikey=${publicKey}&hash=${token}`;
+const ts = new Date().getTime();
+const stringToHash = ts + privateKey + publicKey;
+const token = md5(stringToHash);
+const BASE_URL = 'https://gateway.marvel.com:443/v1/public/characters?name=thor';
+const testUrl = BASE_URL + '&ts=' + ts + '&apikey=' + publicKey + '&hash=' + token;
+
 
 // const { typeDefs, resolvers } = require('./schemas');
 // const { authMiddleware } = require('./utils/auth');
 // const db = require('./config/connection');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+console.log(testUrl);
+console.log(ts);
+console.log(token);
 
-app.get('/',async (req, res) => {
- let result = await axios.get(testUrl)
- .then (function (res) {
-  res.json(result);
- });
+// app.get('/home',async (req, res) => {
+// fetch(testUrl).then(response => {
+//   response.json()
+// })
+// .then (data => {
+//   console.log(data);
+// })
+// });
+app.get('/home',async (req, res) => {
+ const result = await axios.get(testUrl);
+ console.log(result);
 })
+
 
 // const startServer = async () => {
 //   const server = new ApolloServer({
