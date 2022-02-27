64cc2170aa108b5d9a16c508881d155ed985a6d3
taylorkeltgen <taylorkeltgen@gmail.com>
2022-02-27 16:53:05 -0600

    added utils to React, links starter code for client-side database, and starter React-rounter version 5 with simple routes

--- client/src/components/FeaturedHeroSpotlight/index.js
+++ client/src/components/FeaturedHeroSpotlight/index.js
@@ -1,33 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_HEROES } from '../utils/queries';
 
-const FeaturedHero = ({heroData}) => {
+const FeaturedHero = ({ heroData }) => {
+  // use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_HEROES);
+  const heroes = data?.heroes || [];
+  console.log(heroes);
+
   return (
     <div>
-        <div key={heroData.id} className="card">
-            <div className="hero-card-container">
-                    <div className="card-body">
-                        <h3>Featured Hero</h3>
-                        <h2 className="hero-name">{heroData.name}</h2>
-                        <div className="hero-card">
-                            <p className="secondary-text">Bio:</p>
-                            <p className="secondary-text">{heroData.bio}</p>
-                        </div>
-                    </div>
-                    <div className="hero-image card-body">
-                        <img
-                            src={heroData.image}
-                            alt={heroData.name}
-                            className="card-img-top"
-                        />
-                    </div>
-                    <div className='hero-trivia'>
-                        <p>Did you know..?</p>
-                        <p>{heroData.trivia}</p>
-                    </div>
+      <div key={heroData.id} className="card">
+        <div className="hero-card-container">
+          <div className="card-body">
+            <h3>Featured Hero</h3>
+            <h2 className="hero-name">{heroData.name}</h2>
+            <div className="hero-card">
+              <p className="secondary-text">Bio:</p>
+              <p className="secondary-text">{heroData.bio}</p>
             </div>
+          </div>
+          <div className="hero-image card-body">
+            <img src={heroData.image} alt={heroData.name} className="card-img-top" />
+          </div>
+          <div className="hero-trivia">
+            <p>Did you know..?</p>
+            <p>{heroData.trivia}</p>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FeaturedHero
+export default FeaturedHero;
