694c848fe7956a0aca89fe2b5172ed4d18c2afb0
sabrinaelkins <sabrina.elkins12@gmail.com>
2022-02-28 15:59:39 -0600

    updates nav

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -19,50 +19,53 @@
 //     </Container>
 //   </Navbar>
 
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
 
-function TempNav() {
-  return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand>
-        {' '}
-        <Link data-testid="about" to="/" className="nav-link">
-          <h1 className="page-title">Marvelous</h1>
-        </Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <div className="container">
-          <nav className="navbar navbar-expand-md navbar-dark">
-            <ul className="navbar-nav ms-auto">
-              <li>
-                <Link data-testid="about" to="/" className="nav-link">
-                  <h5 className="pink text-nowrap">Home</h5>
-                </Link>
-              </li>
-              <li>
-                <Link data-testid="search" to="/search" className="nav-link">
-                  <h5 className="pink">Search</h5>
-                </Link>
-              </li>
-              <li>
-                <Link data-testid="contact" to="/spotlight" className="nav-link">
-                  <h5 className="pink">Spotlight</h5>
-                </Link>
-              </li>
-              <li>
-                <Link data-testid="resume" to="/login" className="nav-link">
-                  <h5 className="pink">Login</h5>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-}
 
-export default TempNav;
+
+// import React from 'react';
+// import Navbar from 'react-bootstrap/Navbar';
+// import { Link } from 'react-router-dom';
+
+// function TempNav() {
+//   return (
+//     <Navbar bg="dark" variant="dark" expand="lg">
+//       <Navbar.Brand>
+//         {' '}
+//         <Link data-testid="about" to="/" className="nav-link">
+//           <h1 className="page-title">Marvelous</h1>
+//         </Link>
+//       </Navbar.Brand>
+//       <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//       <Navbar.Collapse id="basic-navbar-nav">
+//         <div className="container">
+//           <nav className="navbar navbar-expand-md navbar-dark">
+//             <ul className="navbar-nav ms-auto">
+//               <li>
+//                 <Link data-testid="about" to="/" className="nav-link">
+//                   <h5 className="pink text-nowrap">Home</h5>
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link data-testid="search" to="/search" className="nav-link">
+//                   <h5 className="pink">Search</h5>
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link data-testid="contact" to="/spotlight" className="nav-link">
+//                   <h5 className="pink">Spotlight</h5>
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link data-testid="resume" to="/login" className="nav-link">
+//                   <h5 className="pink">Login</h5>
+//                 </Link>
+//               </li>
+//             </ul>
+//           </nav>
+//         </div>
+//       </Navbar.Collapse>
+//     </Navbar>
+//   );
+// }
+
+// export default TempNav;
