f94d91699468d685c65107d56fc6db367ed2aa32
Zak <zak.krutikov@gmail.com>
2022-02-28 14:13:47 -0500

    added data wrapper for api calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // COMPONENT IMPORTS //
-import Footer from './components/Footer';
-import HeroSearch from './pages/HeroSearch';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Spotlight from './pages/Spotlight';
-import TempNav from './components/TempNav';
+import Footer from "./components/Footer";
+import HeroSearch from "./pages/HeroSearch";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Spotlight from "./pages/Spotlight";
+import TempNav from "./components/TempNav";
 
 /// App Setup ///
 function App() {
--- client/src/pages/HeroSearch.js
+++ client/src/pages/HeroSearch.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Search from '../components/Search';
+import React from "react";
+import Search from "../components/Search";
 
 const HeroSearch = () => {
   return (
