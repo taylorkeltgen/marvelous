e266eee2e8d5eeb54adf334d819ad2247d35d796
Bryan A <bryanacostadelacruz@gmail.com>
2022-03-01 06:04:34 -0600

    single hero component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 // COMPONENT IMPORTS //
 import Footer from "./components/Footer";
 import HeroSearch from "./pages/HeroSearch";
@@ -10,24 +9,22 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Spotlight from "./pages/Spotlight";
 import NavBar from "./components/NavBar";
-import HeroPage from "./pages/Hero";
+import SingleHero from "./components/SingleHero";
 /// App Setup ///
 function App() {
 	return (
 		<Router>
-			<>
-				<NavBar />
+			<NavBar />
 
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/signup" component={Signup} />
-					<Route exact path="/search" component={HeroSearch} />
-					<Route exact path="/spotlight" component={Spotlight} />
-					<Route exact path="/singlehero" component={HeroPage} />
-				</Switch>
-				<Footer />
-			</>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/signup" component={Signup} />
+				<Route exact path="/search" component={HeroSearch} />
+				<Route exact path="/spotlight" component={Spotlight} />
+				<Route path="/singlehero" component={SingleHero} />
+			</Switch>
+			<Footer />
 		</Router>
 	);
 }
--- client/src/components/SingleHero/index.js
+++ client/src/components/SingleHero/index.js
@@ -1,11 +1,31 @@
-import React from "react";
-import "./index.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const SingleHero = (HeroData) => {
+// API Call through URL params
+const SingleHero = () => {
+	const [character, setCharacter] = useState({});
+	const queryParams = new URLSearchParams(window.location.search);
+	const id = queryParams.get("id");
+	// declaring variables needed for an API call as per Marvel documentation
+	const md5 = require("md5");
+	const privateKey = "f0b9f04ae0e902fb5e6e3072b6a0f44efd6f2b02";
+	const publicKey = "e6ab680f2e52f24d5817b4a301f88e41";
+	const ts = new Date().getTime();
+	const stringToHash = ts + privateKey + publicKey;
+	const token = md5(stringToHash);
+	const BASE_URL =
+		"https://gateway.marvel.com:443/v1/public/characters?id=" + id;
+	const testUrl =
+		BASE_URL + "&ts=" + ts + "&apikey=" + publicKey + "&hash=" + token;
+	//making API call using axios
+	axios.get(`${testUrl}`).then((res) => {
+		const chardata = res.data.data.results[0];
+		setCharacter(chardata);
+	});
 	return (
 		<div>
-			<h1>{HeroData.name}</h1>
-			<h1>{HeroData.id}</h1>
+			<h1>{character.name}</h1>
+			<h1>{character.description}</h1>
 		</div>
 	);
 };
