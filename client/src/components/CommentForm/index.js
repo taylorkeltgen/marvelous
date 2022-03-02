dbc255a71015e97963babc534c0ad36ba480f1fb
Zak Krutikov <zak.krutikov@gmail.com>
2022-03-02 18:52:26 -0500

    minor visual changes. centered comment button

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { ADD_COMMENT } from "../../utils/mutations";
+import { Container } from 'react-bootstrap';
 
 const CommentForm = (props) => {
 	const [commentText, setText] = useState("");
@@ -33,7 +34,7 @@ const CommentForm = (props) => {
 	return (
 		<div>
 			<form
-				className=" card text-white bg-dark justify-center justify-space-between-md p-2"
+				className=" card text-white bg-dark justify-content-center justify-space-between-md p-2"
 				onSubmit={handleFormSubmit}
 			>
 				<label className="page-title p-2">Share your thoughts!</label>
@@ -43,10 +44,11 @@ const CommentForm = (props) => {
 					value={commentText}
 					onChange={handleChange}
 				/>
-
+<Container>
 				<button className="btn btn-danger col-12 col-md-3 mt-2" type="submit">
 					Comment
 				</button>
+				</Container>
 			</form>
 
 			{error && <div>Something went wrong...</div>}
