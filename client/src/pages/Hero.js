b4b5296415c0488f34567be72f9361cd7357c2d2
Zak Krutikov <zak.krutikov@gmail.com>
2022-03-01 23:51:11 -0500

    set up single hero component

--- client/src/pages/Hero.js
+++ client/src/pages/Hero.js
@@ -3,36 +3,6 @@ import axios from "axios";
 import SingleHero from "../components/SingleHero";
 // API Call through URL params
 const HeroPage = () => {
-<<<<<<< HEAD
-  const [character, setCharacter] = useState([]);
-  const queryParams = new URLSearchParams(window.location.search);
-  const id = queryParams.get("heroname");
-  // declaring variables needed for an API call as per Marvel documentation
-  const md5 = require("md5");
-  const privateKey = "c9e3d0742ac2221695971bb908881f232f6f4a61";
-  const publicKey = "1f3466c186b0df273e3fc7f5e5e11ebc";
-  const ts = new Date().getTime();
-  const stringToHash = ts + privateKey + publicKey;
-  const token = md5(stringToHash);
-  const BASE_URL =
-    "https://gateway.marvel.com:443/v1/public/characters?name=" + id;
-  const testUrl =
-    BASE_URL + "&ts=" + ts + "&apikey=" + publicKey + "&hash=" + token;
-  //making API call using axios
-  console.log(testUrl);
-  useEffect(() => {
-    axios.get(`${testUrl}`).then((res) => {
-      console.log(res);
-      const chardata = res.data.data.results[0];
-      setCharacter(chardata);
-    });
-  }, []);
-  return (
-    <div>
-      <SingleHero HeroData={character} />
-    </div>
-  );
-=======
 	const [character, setCharacter] = useState({});
 	const [image, setImage] = useState({});
 	const queryParams = new URLSearchParams(window.location.search);
@@ -64,7 +34,6 @@ const HeroPage = () => {
 			<SingleHero HeroData={character} HeroImage={image} />
 		</div>
 	);
->>>>>>> d9a152d386b9a49847298924b439d51815cb874f
 };
 
 export default HeroPage;
